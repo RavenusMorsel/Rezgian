9590a60162cb48a7f5f37f87fb6816724632c8ec
Ravenus <sr_shogran_t@hotmail.com>
2026-03-16 21:32:46 -0400

    fix chat and main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
-
 from fastapi.staticfiles import StaticFiles
 from app.routes import rooms, chat
 
@@ -11,4 +10,4 @@ app.mount("/app/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
 
 app.include_router(rooms.router)
-app.include_router(chat.router)
+app.include_router(chat.router, prefix="/api/chat")
