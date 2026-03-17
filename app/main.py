803c312afcaeddf4ccc43ce7a1e625ac13d59366
Ravenus <sr_shogran_t@hotmail.com>
2026-03-16 20:14:59 -0400

    Fix?
    godI hope.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+
+from fastapi.staticfiles import StaticFiles
 from app.routes import rooms, chat
 
 app = FastAPI()
+
+app.mount("/app/static", StaticFiles(directory="app/static"), name="static")
+
+templates = Jinja2Templates(directory="app/templates")
 
 app.include_router(rooms.router)
 app.include_router(chat.router)
